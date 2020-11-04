5de58026650c419c036139d4458729f07bd2b5de
Purukitto <purukitto@gmail.com>
2020-11-04 13:59:23 +0530

    1

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const newLines = 10;
+const editLines = 25;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,25 +9,19 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('"vsstats" is now active!');
 
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('vsstats.checkStatus', function() {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Testing');
+        vscode.window.showInformationMessage(`Lines written today: ${newLines}\nLines edited today: ${editLines}`);
     });
 
     context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
